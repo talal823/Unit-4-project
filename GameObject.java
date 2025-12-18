4e71e76765e4d99a286af3450ce05b210b0274a3
talal823 <1098074@pdsb.net>
2025-12-17 22:51:44 -0500

    added GameObject.java

---
+++ GameObject.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+
+public abstract class GameObject {
+    // Abstract parent class for all falling objects
+    int x, y, width, height;
+
+    GameObject(int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
+    }
+
+    // Returns a rectangle for collision detection
+    Rectangle getRect() {
+        return new Rectangle(x, y, width, height);
+    }
+
+    // Method to define effect on player upon collision (implemented by subclasses)
+    abstract void collisionAffect(GameLogic game);
+
+    // Method to define color of the object (implemented by subclasses)
+    abstract Color getColor();
+}
+
